429601e094bfed27a514f5f3731f2e7b177e9af9
TJ Epperson <tj.epperson070@gmail.com>
2020-03-28 17:25:16 +0900

    set up basic user endpoints

---
+++ server.js
@@ -0,0 +1,43 @@
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const PORT = process.env.PORT || 3005
+
+app.use(cors())
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+const queries = require('./database/queries')
+
+app.get('/test', (req, res) => {
+  res.status(200).send('OK')
+})
+
+app.post('/user/login', async (req, res) => {
+  const userObj = req.body
+  if (!userObj.email || !userObj.password) res.status(401).send('Missing Credentials')
+  else
+  try {
+    const results = await queries.loginUser(userObj)
+    if (results.err) res.status(401).send(results)
+    if (results.id) res.status(202).send(results)
+    res.send(results)
+  } catch (err) {
+    res.status(500).send(err)
+  }
+})
+
+app.post('/user/registration', async (req, res) => {
+  const userObj = req.body
+  if (!userObj.email || !userObj.password) res.status(401).send('Missing Credentials')
+  else
+  try {
+    const results = await queries.registerUser(userObj)
+    res.send(results)
+  } catch (err) {
+    res.status(500).send(err)
+  }
+})
+
+app.listen(PORT, () => console.log(`Listening on ${PORT}`))

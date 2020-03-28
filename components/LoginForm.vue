538b9caf7979f6bf2c2c8f5a9e85e3511fa43c80
TJ Epperson <tj.epperson070@gmail.com>
2020-03-28 17:45:51 +0900

    improve login ux

--- components/LoginForm.vue
+++ components/LoginForm.vue
@@ -2,8 +2,8 @@
   <div class="max-width">
     <v-text-field outlined label="E-Mail" v-model="email"/>
     <v-text-field outlined type="password" label="Password" v-model="password"/>
-    <v-btn outlined min-width="80%" class="margin" @click="login">Login</v-btn>
-    <v-btn outlined nuxt min-width="80%" to="/" class="margin">Back</v-btn>
+    <v-btn outlined :loading="loading" min-width="80%" class="margin" @click="login">Login</v-btn>
+    <v-btn outlined :disabled="loading" nuxt min-width="80%" to="/" class="margin">Back</v-btn>
   </div>
 </template>
 
@@ -11,21 +11,23 @@
 export default {
   data: () => ({
     email: null,
-    password: null
+    password: null,
+    loading: false
   }),
   methods: {
     async login () {
+      this.loading = true
       try {
-        const loginResponse = await this.$axios.post('http://localhost:3005/user/login', { email: this.email, password: this.password })
-        if (loginResponse.status === 200) {
-
-        }
+        const loginResponse = await this.$axios.post(`${process.env.api}/user/login`, { email: this.email, password: this.password })
+        console.log(loginResponse)
       } catch (err) {
         if (err.response) {
           if (err.response.status === 401) console.log('Invalid Credentials')
         } else {
           console.log(err)
         }
+      } finally {
+        this.loading = false
       }
     }
   }

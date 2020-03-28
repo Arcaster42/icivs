cb94e9e950b4fdbbd654b57552a9e44257c3bb0f
TJ Epperson <tj.epperson070@gmail.com>
2020-03-28 23:50:39 +0900

    implement game cycle logic

--- server.js
+++ server.js
@@ -8,7 +8,10 @@ app.use(cors())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
-const queries = require('./database/queries')
+const { registerUser, loginUser } = require('./database/queries')
+const { gameCycle } = require('./server/cycle')
+
+setInterval(gameCycle, 1000)
 
 app.get('/test', (req, res) => {
   res.status(200).send('OK')
@@ -19,7 +22,7 @@ app.post('/user/login', async (req, res) => {
   if (!userObj.email || !userObj.password) res.status(401).send({ err: 'Missing Credentials' })
   else
     try {
-      const results = await queries.loginUser(userObj)
+      const results = await loginUser(userObj)
       if (results.err) res.status(401).send(results)
       else if (results.id) res.status(202).send(results)
       else res.status(500).send(results)
@@ -33,11 +36,15 @@ app.post('/user/registration', async (req, res) => {
   if (!userObj.email || !userObj.password) res.status(401).send({ err: 'Missing Credentials' })
   else
     try {
-      const results = await queries.registerUser(userObj)
+      const results = await registerUser(userObj)
       res.send(results)
     } catch (err) {
       res.status(500).send(err)
     }
+})
+
+app.post('/user/build', async (req, res) => {
+  
 })
 
 app.listen(PORT, () => console.log(`Listening on ${PORT}`))

07397491e260be5c76c73617fe2c8f03ab7c762b
TJ Epperson <tj.epperson070@gmail.com>
2020-03-28 17:24:54 +0900

    configure knex

---
+++ knexfile.js
@@ -0,0 +1,48 @@
+require('dotenv').config()
+
+module.exports = {
+
+  development: {
+    client: 'pg',
+    connection: {
+      host: process.env.DB_HOST,
+      database: process.env.DB_NAME,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      ssl: true
+    }
+  },
+
+  staging: {
+    client: 'pg',
+    connection: {
+      database: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASS
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  },
+
+  production: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  }
+
+};

fd66dd2d79e21fbe97380a32315f87b1218ec136
TJ Epperson <tj.epperson070@gmail.com>
2020-03-28 23:51:08 +0900

    add resources to user schema

--- migrations/20200328164023_user.js
+++ migrations/20200328164023_user.js
@@ -1,10 +1,18 @@
 exports.up = function(knex) {
   return knex.schema.createTable('civ_users', (t) => {
-    t.increments('id').primary()
-    t.string('email').notNull()
+    t.string('email').primary()
     t.string('pass_hash')
     t.timestamp('join_date').defaultTo(knex.fn.now())
     t.string('token')
+    t.json('buildings')
+    t.integer('money').defaultTo(1000)
+    t.integer('free_pop').defaultTo(10)
+    t.integer('food').defaultTo(50)
+    t.integer('wood').defaultTo(20)
+    t.integer('iron').defaultTo(0)
+    t.integer('stone').defaultTo(0)
+    t.integer('gold').defaultTo(0)
+    t.integer('arms').defaultTo(0)
   })
 }
 

333b89da14aa71a95f1b368a0a1def21de0d2ad2
TJ Epperson <tj.epperson070@gmail.com>
2020-03-28 17:26:18 +0900

    create basic splash page

---
+++ pages/index.vue
@@ -0,0 +1,67 @@
+<template>
+  <v-container>
+    <div>
+      <app-logo/>
+      <h1 class="title">
+        iCivs
+      </h1>
+      <h2 class="subtitle">
+        Pocket Civilization
+      </h2>
+      <v-row>
+        <v-col>
+          <v-btn outlined nuxt width="80%" to="form">Login</v-btn>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col>
+          <v-btn outlined width="80%">Register</v-btn>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col>
+          <v-btn outlined nuxt width="80%" to="about">About</v-btn>
+        </v-col>
+      </v-row>
+    </div>
+  </v-container>
+</template>
+
+<script>
+import AppLogo from '~/components/AppLogo.vue'
+
+export default {
+  components: {
+    AppLogo
+  }
+}
+</script>
+
+<style>
+.container {
+  min-height: 95vh;
+  max-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center !important;
+  text-align: center;
+}
+
+.title {
+  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 42px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+</style>
+

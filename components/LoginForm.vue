d0ee513d685fb5ae285a02c65edc34d7232db5aa
TJ Epperson <tj.epperson070@gmail.com>
2020-03-28 19:21:19 +0900

    improve ux for login errors

--- components/LoginForm.vue
+++ components/LoginForm.vue
@@ -1,9 +1,12 @@
 <template>
   <div class="max-width">
     <v-text-field outlined label="E-Mail" v-model="email"/>
-    <v-text-field outlined type="password" label="Password" v-model="password"/>
+    <v-text-field outlined type="password" label="Password" v-model="password" @keypress.enter="login"/>
     <v-btn outlined :loading="loading" min-width="80%" class="margin" @click="login">Login</v-btn>
     <v-btn outlined :disabled="loading" nuxt min-width="80%" to="/" class="margin">Back</v-btn>
+    <v-snackbar color="error" v-model="showError">
+      {{ error }}
+    </v-snackbar>
   </div>
 </template>
 
@@ -12,17 +15,27 @@ export default {
   data: () => ({
     email: null,
     password: null,
-    loading: false
+    loading: false,
+    error: null,
+    showError: false
   }),
   methods: {
     async login () {
       this.loading = true
       try {
-        const loginResponse = await this.$axios.post(`${process.env.api}/user/login`, { email: this.email, password: this.password })
-        console.log(loginResponse)
+        const result = await this.$store.dispatch('LOGIN', { email: this.email, password: this.password })
+        if (result.err) {
+          this.error = result.err
+        }
+        if (result.success) {
+          this.$router.push('/')
+        }
       } catch (err) {
         if (err.response) {
-          if (err.response.status === 401) console.log('Invalid Credentials')
+          if (err.response.status === 401) {
+            this.error = err.response.data.err
+            this.showError = true
+          }
         } else {
           console.log(err)
         }

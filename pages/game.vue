9f7fd65daaa072704c00c00716d9e7836b34b012
TJ Epperson <tj.epperson070@gmail.com>
2020-03-29 15:27:21 +0900

    add game parent component

---
+++ pages/game.vue
@@ -0,0 +1,34 @@
+<template>
+  <v-content>
+    <v-btn icon @click="toggleNav"><v-icon>mdi-menu</v-icon></v-btn>
+    <Navigation :show="showNav" @setNav="setNav"/>
+    <BuildMenu/>
+  </v-content>
+</template>
+
+<script>
+import Navigation from '~/components/Navigation.vue'
+import BuildMenu from '~/components/BuildMenu.vue'
+
+export default {
+  components: {
+    Navigation,
+    BuildMenu
+  },
+  data: () => ({
+    showNav: false
+  }),
+  methods: {
+    toggleNav () {
+      this.showNav = !this.showNav
+    },
+    setNav (value) {
+      this.showNav = value
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>

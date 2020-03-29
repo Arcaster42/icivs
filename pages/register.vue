3b8c903b1c01cb9d2d2ae290ca43457667478548
TJ Epperson <tj.epperson070@gmail.com>
2020-03-29 10:08:06 +0900

    create register view

---
+++ pages/register.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-container fluid>
+    <RegisterForm/>
+  </v-container>
+</template>
+
+<script>
+import RegisterForm from '~/components/RegisterForm.vue'
+
+export default {
+  components: {
+    RegisterForm
+  }
+}
+</script>
+
+<style>
+.container {
+  min-height: 95vh;
+  max-height: 100vh;
+  justify-items: center;
+  align-items: center;
+  display: grid;
+}
+</style>

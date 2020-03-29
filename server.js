10e1205a7f41c5bf73d15d53069f7cba83275118
TJ Epperson <tj.epperson070@gmail.com>
2020-03-29 12:17:25 +0900

    compile ts

---
+++ server.js
@@ -0,0 +1,24 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+// Server Setup
+var express_1 = __importDefault(require("express"));
+var app = express_1.default();
+var body_parser_1 = __importDefault(require("body-parser"));
+var cors_1 = __importDefault(require("cors"));
+var PORT = process.env.PORT || 3005;
+// Middleware
+app.use(cors_1.default());
+app.use(body_parser_1.default.json());
+app.use(body_parser_1.default.urlencoded({ extended: true }));
+// Routes
+var auth_1 = __importDefault(require("./server/routes/auth"));
+var game_1 = __importDefault(require("./server/routes/game"));
+// Game Cycle
+var cycle_1 = require("./server/cycle");
+setInterval(cycle_1.gameCycle, 10000);
+app.use('/user', auth_1.default);
+app.use('/game', game_1.default);
+app.listen(PORT, function () { return console.log("Listening on " + PORT); });

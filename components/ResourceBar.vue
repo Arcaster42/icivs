45b3c69d0898520fe88b26ec0e130ecd28aad228
TJ Epperson <tj.epperson070@gmail.com>
2020-03-29 16:28:21 +0900

    replace text with icons

--- components/ResourceBar.vue
+++ components/ResourceBar.vue
@@ -1,7 +1,17 @@
 <template>
-  <v-sheet color="light-blue" class="mt-2">
-    <span>Food: {{ resources.food }} -- Wood: {{ resources.wood }} -- Stone: {{ resources.stone }} -- Iron: {{ resources.iron }} -- Gold: {{ resources.gold }}</span><br>
-    <span>Money: {{ resources.money }} -- Population: {{ resources.free_pop }}</span>
+  <v-sheet class="mt-2 ml-2 mr-2 text-right">
+    <span>
+      <v-icon color="green">mdi-cash</v-icon>{{ resources.money }}
+      <v-icon color="brown lighten-3">mdi-account-group</v-icon>{{ resources.free_pop }}
+    </span>
+    <br>
+    <span>
+      <v-icon color="red darken-4">mdi-food-apple</v-icon>{{ resources.food }}
+      <v-icon color="brown">mdi-pine-tree</v-icon> {{ resources.wood }}
+      <v-icon color="grey">mdi-image-filter-hdr</v-icon> {{ resources.stone }}
+      <v-icon color="orange darken-3">mdi-image-filter-hdr</v-icon> {{ resources.iron }}
+      <v-icon color="yellow">mdi-image-filter-hdr</v-icon>{{ resources.gold }}
+    </span>
   </v-sheet>
 </template>
 

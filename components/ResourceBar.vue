5b65573cbb72f2d4bcb1e120a6214a6895f16860
TJ Epperson <tj.epperson070@gmail.com>
2020-03-29 16:10:30 +0900

    add first iteration resource bar

---
+++ components/ResourceBar.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-sheet color="light-blue">
+    <span>Food: {{ resources.food }} -- Wood: {{ resources.wood }} -- Stone: {{ resources.stone }} -- Iron: {{ resources.iron }} -- Gold: {{ resources.gold }}</span><br>
+    <span>Money: {{ resources.money }} -- Population: {{ resources.free_pop }}</span>
+  </v-sheet>
+</template>
+
+<script>
+export default {
+  computed: {
+    resources () {
+      return this.$store.getters.GET_USER_RESOURCES
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>

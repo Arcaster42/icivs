96477204b96b855b22172f1017213ccd9e683adf
TJ Epperson <tj.epperson070@gmail.com>
2020-03-30 22:44:44 +0900

    implement summary navigation

--- components/Navigation.vue
+++ components/Navigation.vue
@@ -1,7 +1,11 @@
 <template>
   <v-navigation-drawer mini-variant absolute v-model="showNav" @input="emitNav">
     <v-list-item v-for="item of items" :key="item.title" @click="item.click">
-      <v-list-item-icon><v-icon>{{ item.icon }}</v-icon></v-list-item-icon>
+      <v-list-item-icon>
+        <!-- <v-badge color="error" :content="constructionCount" offset-x="8" offset-y="8" v-if="item.title==='Construction' && constructionCount"> -->
+          <v-icon color="primary">{{ item.icon }}</v-icon>
+        <!-- </v-badge> -->
+      </v-list-item-icon>
       <v-list-item-title>{{ item.title }}</v-list-item-title>
     </v-list-item>
   </v-navigation-drawer>
@@ -9,16 +13,26 @@
 
 <script>
 export default {
+  async mounted () {
+    try {
+      const constructions = await this.$store.dispatch('CONSTRUCTION')
+      this.constructionCount = constructions.length
+    } catch (err) {
+      console.log(err)
+    }
+  },
   props: ['show'],
   data: function () {
     return {
       showNav: this.show,
       items: [
+        { icon: 'mdi-home-group', title: 'Summary', click: this.openSummary },
         { icon: 'mdi-hammer', title: 'Build', click: this.openBuild },
         { icon: 'mdi-domain', title: 'Construction', click: this.openConstruction },
         { icon: 'mdi-help', title: 'Help', click: this.openHelp },
         { icon: 'mdi-logout', title: 'Logout', click: this.logout }
-      ]
+      ],
+      constructionCount: null
     }
   },
   watch: {
@@ -29,6 +43,10 @@ export default {
   methods: {
     emitNav (isOpen) {
       this.$emit('setNav', isOpen)
+    },
+    openSummary () {
+      this.showNav = false
+      this.$emit('setView', 'SummaryMenu')
     },
     openBuild () {
       this.showNav = false

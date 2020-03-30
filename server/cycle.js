221c52c596ec41acc91c114d53f2cfec0495d144
TJ Epperson <tj.epperson070@gmail.com>
2020-03-30 11:56:49 +0900

    add construction cycle logic

--- server/cycle.js
+++ server/cycle.js
@@ -1,16 +1,19 @@
-const { getAllUsers, cycleUpdate } = require('../database/queries')
+const moment = require('moment')
+const { getAllUsers, cycleConstruction, cycleUpdate } = require('../database/queries')
 
 const gameCycle = async () => {
+  // Add Resources
   try {
     const allUsers = await getAllUsers()
     if (allUsers.length > 0) {
       for (const user of allUsers) {
-        const buildings = JSON.parse(user.buildings)
+        const buildings = user.buildings
         if (!buildings) continue
         if (buildings.farm) user.food += 5 * buildings.farm
+        if (buildings.lumber_yard) user.wood += 5 * buildings.lumber_yard
         if (buildings.house) user.free_pop += 1 * buildings.house
         if (buildings.iron_mine) user.iron += 1 * buildings.iron_mine
-        if (buildings.quarry) user.quarry += 1 * buildings.quarry
+        if (buildings.quarry) user.stone += 1 * buildings.quarry
         if (buildings.market) {
           user.free_pop += 1 * buildings.market
           user.money += 100 * buildings.market
@@ -23,6 +26,15 @@ const gameCycle = async () => {
   } catch (err) {
     console.log(err)
   }
+
+  // Cycle Construction
+  try {
+    await cycleConstruction()
+  } catch (err) {
+    console.log(err)
+  }
+
+  console.log(`${moment()} -- Cycle`)
 }
 
 module.exports = {

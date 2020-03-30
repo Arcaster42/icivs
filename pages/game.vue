fa33339ff03c4d1ae17d3d0fd0d4d784429cd7a6
TJ Epperson <tj.epperson070@gmail.com>
2020-03-30 17:07:57 +0900

    implement construction menu, improve titles

--- pages/game.vue
+++ pages/game.vue
@@ -2,11 +2,12 @@
   <v-content>
     <v-row class="mt-2 ml-2 mr-2" justify="space-between">
       <v-btn icon @click="toggleNav"><v-icon x-large>mdi-menu</v-icon></v-btn>
-      <h1>{{ view }}</h1>
+      <h1>{{ viewDict[view] }}</h1>
     </v-row>
     <Navigation :show="showNav" @setNav="setNav" @setView="setView"/>
     <ResourceBar/>
     <BuildMenu v-if="view==='BuildMenu'"/>
+    <ConstructionMenu v-if="view==='ConstructionMenu'"/>
   </v-content>
 </template>
 
@@ -14,16 +15,23 @@
 import Navigation from '~/components/Navigation.vue'
 import ResourceBar from '~/components/ResourceBar.vue'
 import BuildMenu from '~/components/BuildMenu.vue'
+import ConstructionMenu from '~/components/ConstructionMenu.vue'
 
 export default {
   components: {
     Navigation,
     ResourceBar,
-    BuildMenu
+    BuildMenu,
+    ConstructionMenu
   },
   data: () => ({
     showNav: false,
-    view: null
+    view: 'Summary',
+    viewDict: {
+      'Summary': 'Summary',
+      'BuildMenu': 'New Construction',
+      'ConstructionMenu': 'Current Construction'
+    }
   }),
   methods: {
     toggleNav () {
